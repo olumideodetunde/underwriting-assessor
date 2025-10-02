8b6e0a6eeb46c2da78ce5494519f9e7772770609
Olumide Odetunde <odetundeolumide95@gmail.com>
2025-10-02 20:53:03 +0100

    update: fast api endpoint functional

--- src/app/freq.py
+++ src/app/freq.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#TODO - make the endpoint work - may need to be sorted from s3 bucket path
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
