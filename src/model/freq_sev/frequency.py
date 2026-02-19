5d3e9e4a8411a08eb8f50e778e25943f6632e6ed
Olumide Odetunde <odetundeolumide95@gmail.com>
2026-02-19 08:58:31 +0000

    update: refactor frequency.py to use get_frequency_config and improve model training logging

--- src/model/freq_sev/frequency.py
+++ src/model/freq_sev/frequency.py
@@ -1,15 +1,16 @@
+import os
+
 import mlflow
 from sklearn.linear_model import PoissonRegressor
 from src.model.freq_sev.dataset import main as dataset_prep_main
 from src.model.freq_sev.feature import main as feature_eng_main
-from src.model.freq_sev.utils import register_and_upload_model, get_config
+from src.model.freq_sev.utils import register_and_upload_model, get_frequency_config
 from src.model.freq_sev.eval import (plot_claims_distribution,
                                      plot_feature_importance,
                                      plot_residuals,
                                      plot_actual_vs_predicted,
                                      calculate_metrics)
-
-CONFIG = get_config()
+CONFIG = get_frequency_config()
 mlflow.set_tracking_uri(CONFIG["MLFLOW_TRACKING_URI"])
 mlflow.set_experiment(CONFIG["EXPERIMENT_NAME"])
 insurance_variables_path = CONFIG["INSURANCE_VARIABLES_PATH"]
@@ -19,8 +20,10 @@ def main(config):
     train, test = dataset_prep_main(config["INSURANCE_VARIABLES_PATH"], config["CLAIMS_VARIABLES_PATH"])
     train_with_eng_feature = feature_eng_main(train)
     test_with_eng_feature = feature_eng_main(test)
-    training_variables = config.get('TRAINING_VARIABLES')
-    target = config.get('TARGET')
+    training_variables = config.get('FREQ_FEATURES').split(',')
+    target = config.get('FREQ_TARGET')
+
+
     fig1 = plot_claims_distribution(train_with_eng_feature)
     mlflow.set_tracking_uri(config["MLFLOW_TRACKING_URI"])
     mlflow.set_experiment(config["EXPERIMENT_NAME"])
@@ -30,6 +33,13 @@ def main(config):
     with mlflow.start_run(run_name=run_name, description=run_description) as run:
         run_id = run.info.run_id
         print(f"Started MLflow run: {run_id}")
+
+        train_dataset = mlflow.data.from_pandas(train_with_eng_feature, targets=target)
+        test_dataset = mlflow.data.from_pandas(test_with_eng_feature, targets=target)
+        mlflow.log_input(train_dataset, context="training")
+        mlflow.log_input(test_dataset, context="testing")
+
+
         poisson_regressor = PoissonRegressor(alpha=1e-12, solver='newton-cholesky', max_iter=300)
         poisson_model = poisson_regressor.fit(
             train_with_eng_feature[training_variables],
@@ -54,19 +64,25 @@ def main(config):
             test_with_eng_feature[target].values.ravel(),
             y_pred_test
         )
-        mlflow.log_param("alpha", poisson_regressor.alpha)
-        mlflow.log_param("solver", poisson_regressor.solver)
-        mlflow.log_param("max_iter", poisson_regressor.max_iter)
-        mlflow.log_param("run_name", run_name)
-        mlflow.log_param("train_variables", training_variables)
-        mlflow.log_param("model_name", config["MODEL_NAME"])
+        mlflow.log_params(
+            {
+                'alpha': poisson_model.alpha,
+                'solver': poisson_model.solver,
+                'max_iter': poisson_model.max_iter,
+                'run_name': run_name,
+                'training_variables': training_variables,
+                'target': target,
+                'model_name': config["MODEL_NAME"],
+
+            }
+        )
         for name, value in train_metrics.items():
             mlflow.log_metric(f"train/{name}", value)
         for name, value in test_metrics.items():
             mlflow.log_metric(f"test/{name}", value)
         mlflow.sklearn.log_model(
             sk_model=poisson_model,
-            artifact_path=config["ARTIFACT_MODEL_NAME"],
+            name=config["ARTIFACT_MODEL_NAME"],
             input_example=test_with_eng_feature[training_variables].head(5)
         )
         mlflow.log_figure(fig1, "claims_distribution.png")
@@ -74,17 +90,18 @@ def main(config):
         mlflow.log_figure(fig3, "actual_vs_predicted.png")
         mlflow.log_figure(fig4, "residuals.png")
         print(f"✓ Model training completed")
+
     # After the run completes, register and upload the model
-    print("\n" + "=" * 50)
-    print("Registering and uploading model...")
-    print("=" * 50)
-    version = register_and_upload_model(config, run_id)
-    print("\n" + "=" * 50)
-    print(f"✓ Process completed successfully!")
-    print(f"Model: {config['MODEL_NAME']} v{version}")
-    print(f"Run ID: {run_id}")
-    print("=" * 50)
-    return run_id, version
+    # print("\n" + "=" * 50)
+    # print("Registering and uploading model...")
+    # print("=" * 50)
+    # version = register_and_upload_model(config, run_id)
+    # print("\n" + "=" * 50)
+    # print(f"✓ Process completed successfully!")
+    # print(f"Model: {config['MODEL_NAME']} v{version}")
+    # print(f"Run ID: {run_id}")
+    # print("=" * 50)
+    return run_id  #, version
 if __name__ == "__main__":
-    run_id, version = main(CONFIG)
+    run_id = main(CONFIG)
 
